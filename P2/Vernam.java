f1c229d2f37c03d0de4a2662c076d04cd99e71b1
asp977 <Akashpagi07@gmail.com>
2022-08-12 21:56:48 +0530

    removing comments

---
+++ P2/Vernam.java
@@ -0,0 +1,67 @@
+import java.io.*;
+public class Vernam {
+	
+	public static String stringEncryption(String text,String key)
+	{
+		
+		String cipherText = "";
+		int cipher[] = new int[key.length()];
+		for (int i = 0; i < key.length(); i++) 
+		{
+		cipher[i] = text.charAt(i) - 'A'+ key.charAt(i)- 'A';
+		}
+
+		for (int i = 0; i < key.length(); i++) 
+		{
+		if (cipher[i] > 25) 
+			{
+			cipher[i] = cipher[i] - 26;
+			}
+		}
+		
+		for (int i = 0; i < key.length(); i++) 
+		{
+		int x = cipher[i] + 'A';
+		cipherText += (char)x;
+		}
+		return cipherText;
+	}
+
+	
+	// Returning plain text
+	public static String stringDecryption(String s,String key)
+	{
+		
+		String plainText = "";
+		int plain[] = new int[key.length()];
+		for (int i = 0; i < key.length(); i++) 
+		{
+		plain[i]= s.charAt(i) - 'A' - (key.charAt(i) - 'A');
+		}
+
+		for (int i = 0; i < key.length(); i++) {
+			if (plain[i] < 0) 
+			{
+			plain[i] = plain[i] + 26;
+			}
+		}
+
+		for (int i = 0; i < key.length(); i++) 
+		{
+		int x = plain[i] + 'A';
+		plainText += (char)x;
+		}
+
+		// Returning plainText
+		return plainText;
+	}
+
+	public static void main(String[] args)
+	{
+		String plainText = "akash";
+		String key = "MONEY";
+		String encryptedText = stringEncryption(plainText.toUpperCase(), key.toUpperCase());
+		System.out.println("Cipher Text - "+ encryptedText);
+		System.out.println("Message - "+ stringDecryption(encryptedText,key.toUpperCase()));
+	}
+}
